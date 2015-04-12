16ddded466bebf486c1ef8a6308ff8b42a65effa
stevenmiller888 <stevenmiller888@me.com>
2015-04-12 00:00:07 -0700

    updated test/helpers.js

--- test/helpers.js
+++ test/helpers.js
@@ -5,11 +5,23 @@
 
 var exec = require('child_process').exec;
 
+/**
+ * Set up the git repository.
+ *
+ * @param {Function} cb
+ */
+
 exports.setup = function(cb){
   exec('mkdir /tmp/gity', function(){
     exec('touch /tmp/gity/index.js', cb);
   });
 };
+
+/**
+ * Clean up the git repository.
+ *
+ * @param {Function} cb
+ */
 
 exports.cleanup = function(cb){
   exec('rm -rf /tmp/gity', cb);
